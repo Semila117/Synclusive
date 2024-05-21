aa9b340c3530e9cc22d6cee3c427b2032850be35
semilacapitao <semilaneto@gmail.com>
2024-05-21 17:46:06 +0100

    Novo Exerci

---
+++ Exerc02.py
@@ -0,0 +1,9 @@
+nome = "Semila"
+idade = "25"
+sexo = "Feminino"
+nacionalidade = "Angolana"
+
+print(f"Meu nome é {nome}.")
+print(f"Minha idade é {idade} anos")
+print(f"Meu sexo é {sexo}")
+print(f"Minha nacionalidade é {nacionalidade}")
