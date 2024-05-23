9bbfbbc088cdf8c44ade7457ee3a56f67374a00b
semilacapitao <semilaneto@gmail.com>
2024-05-23 02:16:10 +0100

    update

---
+++ Exerc03.py
@@ -0,0 +1,5 @@
+pi = 3.14
+vari = 4.6
+
+resultado = pi * vari
+print(f"O resultado da multiplicação é : {resultado}")
---
+++ Exerc04.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+resultado_soma = num1 + num2
+resultado_multi = num1 * num2
+resultado_subtr = num1 - num2
+resultado_divi = num1 / num2
+print("A soma é: ",  resultado_soma, '\n' "A multiplicação é: ", resultado_multi, '\n' "A subtração é: ", resultado_subtr, '\n' "A divisão é: ", resultado_divi, '\n')
